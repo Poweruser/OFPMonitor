1680c93466dcee77a8115afddaf9c1045afaca57
Poweruser <poweruser.rs@hotmail.com>
2010-03-19 02:51:19 +0100

    v1.04

--- OFPMonitor.cpp
+++ OFPMonitor.cpp
@@ -28,7 +28,7 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	try
         {
                  Application->Initialize();
-                 Application->Title = "OFPMonitor 1.031";
+                 Application->Title = "OFPMonitor 1.04";
                  Application->CreateForm(__classid(TForm1), &Form1);
                  Application->CreateForm(__classid(TForm2), &Form2);
                  Application->Run();
